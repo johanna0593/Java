aa06a3f3bb1b334a63e7f608546a7873c1633423
Johanna Liza <johanna.herrera@al.infnet.edu.br>
2025-03-30 01:30:36 -0300

    Exercicio_12 done

--- AT/src/main/java/exercicio_01/exercicio_01.java
+++ AT/src/main/java/exercicio_01/exercicio_01.java
@@ -2,6 +2,6 @@ package exercicio_01;
 
 public class exercicio_01 {
     public static void Start() {
-        System.out.println("Olá, meu nome é Johanna e estou aprendendo Java!");
+        System.out.println("Olá, meu nome é Johanna Liza e estou aprendendo Java!");
     }
 }
